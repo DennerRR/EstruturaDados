1e2f4224aa802d23c5daf4904771d74eb8cae1b7
Willian Giovanni <willianpires75@gmail.com>
2020-12-17 01:47:15 -0300

    Mais vetor

--- EstruturaDeDados/src/App/Main.java
+++ EstruturaDeDados/src/App/Main.java
@@ -1,14 +1,22 @@
 package App;
 
+import java.lang.reflect.Array;
+import java.util.Collections;
+
 import Classes.Vetor;
 import Estudante.Estudante;
 
 public class Main {
     public static void main(String[] args) {
-        Vetor v = new Vetor<>();
-        Estudante teste = new Estudante();
+        Vetor v = new Vetor();
+        Estudante e = new Estudante();
+        Vetor teste = new Vetor();
+        
+        
+        System.out.println(v.adicionarEstudante(e));
+        System.out.println(v.contadorEs(e));
+        
 
-        v.put(teste, 100000);
 
     }
 }
